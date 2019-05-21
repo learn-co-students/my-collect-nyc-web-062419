af87af6c8378e54d6e6696987f24fed68b957ec5
Colin <cstatro@gmail.com>
2019-05-21 00:31:10 -0400

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,14 @@
-
+def my_collect array
+    if array.length > 0
+        i = 0
+        new_collection = []
+        while i < array.length
+            new_collection.push(yield array[i])
+            i += 1
+        end
+        new_collection
+    else
+    "Nope"
+    end
+end
 
