595c0d50c5a4a1a19e572342ec536ffade78701b
Anthony <Anthonywan00@gmail.com>
2019-05-31 19:14:00 -0400

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,14 @@
-
+def my_collect(arr)
+    if block_given?
+        i = 0 
+        x = []
+        while i < arr.length
+            x.push(yield arr[i])
+            i += 1
+        end
+    else
+        return "No block is given"
+    end
+    x
+end
 
