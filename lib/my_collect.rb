3debe263b3bc9494b6bbb6ba0b534fd140427684
IanGrubb <yangrubb@gmail.com>
2019-06-15 00:29:08 -0400

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,17 @@
 
+def my_collect(array)
+
+    counter = 0
+    updated = []
+
+    while counter < array.size do
+
+        updated.push( yield array[counter])
+
+        counter +=1
+
+    end
+
+    updated
+end
 
